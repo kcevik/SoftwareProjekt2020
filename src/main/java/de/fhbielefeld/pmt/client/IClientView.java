21090bc06669415d344f33afb84dfb562c1c120c
Siggi <origin.origin@gmx.de>
2020-05-15 20:31:19 +0200

    ClientView kann Projekte anpassen
    Jeder hat nen ReadAllProjects Event ?! Allgemeiner machen 
    Speichern in die DB ohne typsicherheit
    isActive Tests wenn die Projekte angezeigt werden

--- src/main/java/de/fhbielefeld/pmt/client/IClientView.java
+++ src/main/java/de/fhbielefeld/pmt/client/IClientView.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import de.fhbielefeld.pmt.IViewAccessor;
 import de.fhbielefeld.pmt.JPAEntities.Client;
+import de.fhbielefeld.pmt.JPAEntities.Project;
 
 /**
  * Die Interfacedefinition für die View der Clientkomponente.
@@ -13,4 +14,5 @@ public interface IClientView extends IViewAccessor {
 
 	public void setClients(List<Client> clients);
 	public void addClient(Client c);
+	public void setProjects(List<Project> projectListFromDatabase);
 }
