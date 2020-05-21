c7acc7c82b722b1dfe7971dcd70a1f5e4094a8ff
Lucas <lucas.eickmann@fh-bielefeld.de>
2020-05-21 19:12:34 +0200

    PDF Download läuft halbwegs

--- src/main/java/de/fhbielefeld/pmt/projectdetails/impl/view/VaadinProjectdetailsViewLogic.java
+++ src/main/java/de/fhbielefeld/pmt/projectdetails/impl/view/VaadinProjectdetailsViewLogic.java
@@ -31,6 +31,7 @@ import de.fhbielefeld.pmt.JPAEntities.Project;
 import de.fhbielefeld.pmt.JPAEntities.Team;
 import de.fhbielefeld.pmt.converter.plainStringToDoubleConverter;
 import de.fhbielefeld.pmt.moduleChooser.event.ModuleChooserChosenEvent;
+import de.fhbielefeld.pmt.pdf.KostenPDFGenerator;
 import de.fhbielefeld.pmt.pdf.PDFGenerator;
 import de.fhbielefeld.pmt.project.impl.event.ReadAllClientsEvent;
 import de.fhbielefeld.pmt.project.impl.event.ReadAllManagersEvent;
@@ -201,7 +202,7 @@ public class VaadinProjectdetailsViewLogic implements IProjectdetailsView {
 	private void downloadPDF() {
 		
 		Timestamp timeStamp = new Timestamp(System.currentTimeMillis());
-		PDFGenerator gen = new PDFGenerator();
+		KostenPDFGenerator gen = new KostenPDFGenerator();
 		File file = gen.generateTotalCostsPdf(null);
 		StreamResource res = new StreamResource(file.getName(), () ->  {
 			try {
